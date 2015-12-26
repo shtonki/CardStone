d6dad8a3cd068e6f2479a158efc1e9342541f70f
Seba <dese@student.chalmers.se>
2015-12-26 17:58:52 +0100

    patch 0.0.3

--- cardstone/GameAction.cs
+++ cardstone/GameAction.cs
@@ -189,7 +189,7 @@ namespace stonekart
                 cs.Append("'");
             }
             cs.Length--;
-            return "cast" + sw.card.getId() + ';' + sw.card.getAbilityIndex(sw.ability) + ';' + ts + ';' + cs;
+            return "cast," + sw.card.getId() + ';' + sw.card.getAbilityIndex(sw.ability) + ';' + ts + ';' + cs;
         }
     }
     /*
