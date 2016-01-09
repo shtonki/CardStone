62c908e6307da6007db6e6eab43a0549d733c8d0
Seba <dese@student.chalmers.se>
2016-01-09 08:52:27 +0100

    interimx

--- cardstone/MainFrame.cs
+++ cardstone/MainFrame.cs
@@ -311,6 +311,97 @@ namespace stonekart
         {
             heroPanel.showAddMana(b);
         }
+
+        class ButtonPanel : Panel
+        {
+            private ChoiceButton
+                cancel,
+                accept;
+            
+            private Label textLabel;
+
+            public ButtonPanel()
+            {
+                BackColor = Color.CornflowerBlue;
+                Size = new Size(300, 140);
+
+                textLabel = new Label();
+                textLabel.Size = new Size(280, 40);
+                textLabel.Location = new Point(10, 10);
+                textLabel.Font = new Font(new FontFamily("Comic Sans MS"), 14);
+
+                accept = new ChoiceButton(GUI.ACCEPT);
+                accept.Visible = false;
+                accept.BackColor = Color.GhostWhite;
+                accept.Text = "Accept";
+                accept.Font = new Font(new FontFamily("Comic Sans MS"), 12);
+                accept.Location = new Point(40, 100);
+                accept.Click += (sender, args) =>
+                {
+                    buttonPressed(accept);
+                };
+
+                cancel = new ChoiceButton(GUI.CANCEL);
+                cancel.Visible = false;
+                cancel.BackColor = Color.GhostWhite;
+                cancel.Text = "Cancel";
+                cancel.Font = new Font(new FontFamily("Comic Sans MS"), 12);
+                cancel.Location = new Point(140, 100);
+                cancel.Click += (sender, args) =>
+                {
+                    buttonPressed(cancel);
+                };
+
+                Controls.Add(textLabel);
+                Controls.Add(accept);
+                Controls.Add(cancel);
+
+
+            }
+
+            public void setText(string s)
+            {
+                if (s == null)
+                {
+                    return;
+                }
+                Invoke(new Action(() => { textLabel.Text = s; }));
+
+            }
+
+            public void showButtons(int i)
+            {
+                Invoke(new Action(() =>
+                {
+                    accept.Visible = (i & GUI.ACCEPT) != 0;
+                    cancel.Visible = (i & GUI.CANCEL) != 0;
+                }));
+
+            }
+
+            private void buttonPressed(ChoiceButton b)
+            {
+                GameController.currentGame.fooPressed(b);
+            }
+        }
+    }
+
+    
+
+    class ChoiceButton : Button, GameElement
+    {
+        private int type;
+
+        public ChoiceButton(int i)
+        {
+            type = i;
+            Size = new Size(80, 40);
+        }
+
+        public int getType()
+        {
+            return type;
+        }
     }
 
     public class DisplayPanel : Panel
